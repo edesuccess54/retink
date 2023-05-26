cf6c9b77eef422f34a84d237fcf74f5c83f749ca
Ede Success <edesuccess54@gmail.com>
2023-05-26 02:07:40 +0100

    fixed mobile menu

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,30 +7,30 @@ const Footer = ({logo}) => {
     <section className={styles.footerSection}>
         <footer>
             <div className={styles.footerMenu}>
-            <div>
-                <img className={styles.logo} src={logo} alt="logo" />
-                <div className={styles.menu}>
-                    <ul>
-                        <li>Product by Retink Media UG</li>
-                        <li>Bonn, Germany</li>
-                    </ul>
-                    <ul>
-                        <li>Get Early Acess</li>
-                        <li>Provide Feedback</li>
-                    </ul>
+                <div>
+                    <img className={styles.logo} src={logo} alt="logo" />
+                    <div className={styles.menu}>
+                        <ul>
+                            <li>Product by Retink Media UG</li>
+                            <li>Bonn, Germany</li>
+                        </ul>
+                        <ul>
+                            <li>Get Early Acess</li>
+                            <li>Provide Feedback</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.socials}>
-                <p>Connect with Us</p>
-                <ul>
-                    <li className={styles.fb}><FaFacebook /></li>
-                    <li className={styles.tw}><FaTwitter /></li>
-                    <li className={styles.ig}><FaInstagram /></li>
-                    <li className={styles.lin}><AiFillLinkedin /></li>
-                    <li className={styles.yt}><FaYoutube /></li>
-                    <li className={styles.pt}><FaPinterest /></li>
-                </ul>
-            </div>
+                <div className={styles.socials}>
+                    <p>Connect with Us</p>
+                    <ul>
+                        <li className={styles.fb}><FaFacebook /></li>
+                        <li className={styles.tw}><FaTwitter /></li>
+                        <li className={styles.ig}><FaInstagram /></li>
+                        <li className={styles.lin}><AiFillLinkedin /></li>
+                        <li className={styles.yt}><FaYoutube /></li>
+                        <li className={styles.pt}><FaPinterest /></li>
+                    </ul>
+                </div>  
             </div>  
               <div className={styles.copyright}>
                   <p>Copyright © 2021 Retink</p>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,17 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../../assets/images/Logoretinklogo.png';
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { auth, provider } from '../../config'
 import { signOut } from 'firebase/auth';
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { GrClose } from 'react-icons/gr'
+import { useState } from 'react';
 
 
 const Header = () => {
-  const { user, dispatch } = useAuthContext()
+  const { user, dispatch } = useAuthContext();
+  const [ openMenu, setOpenMenu ] = useState(false);
 
   const handleLogout = (e) => {
     e.preventDefault()
@@ -20,12 +24,22 @@ const Header = () => {
         .catch((error) => {
           console.error("Sign-out error:", error);
         });
-
   }
+
+  const handleOpenMenu = (e) => { 
+    setOpenMenu(true)
+  }
+
+  const handleClose = (e) => {
+     setOpenMenu(false)
+   }
   return (
     <div className = {styles.brandWrapper}>
-      <img className={styles.brand} src={logo} alt="" />
-      <nav>
+      <Link to='/'>
+        <img className={styles.brand} src={logo} alt="" />
+      </Link>
+      <span onClick={handleOpenMenu}><GiHamburgerMenu className={ styles.hamburger } /></span>
+      <nav className={styles.desktopMenu}>
         {user && (
           <p style={{display:'inline'}}>{ user }</p>
         )}
@@ -40,6 +54,25 @@ const Header = () => {
           <button onClick={handleLogout} style={{padding: '10px', border: '1px solid #754DE8', backgroundColor:'#fff', borderRadius: '5px', color: '#754DE8', cursor: 'pointer'}}>Log Out</button>
         )}
       </nav>
+
+      {/* mobile menu  */}
+      <nav className={`${styles.mobileNav} ${openMenu ? styles.active : ''}`}>
+        <span onClick={handleClose} className={styles.closeBtn}><GrClose/></span>
+        {user && (
+          <p style={{display:'inline'}}>{ user }</p>
+        )}
+        <NavLink to="/">Home</NavLink>
+        {!user && (
+          <>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/signup">Sign up</NavLink>
+          </>
+        )}
+        {user && (
+          <button onClick={handleLogout} style={{padding: '10px', border: '1px solid #754DE8', backgroundColor:'#fff', borderRadius: '5px', color: '#754DE8', cursor: 'pointer'}}>Log Out</button>
+        )}
+      </nav>
+      <div className={`${styles.overlay} ${openMenu ? styles.show : ''}`}></div>
     </div>
   )
 }
